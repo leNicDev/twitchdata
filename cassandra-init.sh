bc93ebd12d47528b2ccab2c8b158198056196e44
leNicDev <germanelectronix@gmail.com>
2020-09-14 19:54:47 +0200

    Create keyspace when running Cassandra

---
+++ cassandra-init.sh
@@ -0,0 +1,9 @@
+CQL="DROP keyspace twitchdata;
+CREATE KEYSPACE twitchdata WITH replication = {'class': 'SimpleStrategy', 'replication_factor': '2'} AND durable_writes = true;"
+
+until echo $CQL | cqlsh; do
+  echo "cqlsh: Cassandra is unavailable to initialize - will retry later"
+  sleep 2
+done &
+
+exec /docker-entrypoint.sh "$@"
